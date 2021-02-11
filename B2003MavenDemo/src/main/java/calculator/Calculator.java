d05051a3c14980458da995c4f031937121194743
jahidul2543 <jahidul2543@gmail.com>
2021-02-10 21:39:19 -0500

    added maven project

---
+++ B2003MavenDemo/src/main/java/calculator/Calculator.java
@@ -0,0 +1,15 @@
+package calculator;
+
+public class Calculator {
+
+    public int add(int a, int b){
+        int result = a + b;
+        return result;
+    }
+
+    public int deduct(int a, int b){
+        int result = a - b;
+        return result;
+    }
+
+}
